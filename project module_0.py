b6e14d7c4244356fc536ee68261e5f09696dc7a0
Artem Kel <artkel@gmail.com>
2020-09-12 11:15:40 +0200

    Update project module_0.py

--- project module_0.py
+++ project module_0.py
@@ -16,23 +16,27 @@ def score_game(game_core):
 
     return(score)
 
+print("Бинарный поик:")
+def binary_search(number):
+    '''Бинарный поик: Предиктор каждую попытку равен середине диапазона поиска;
+    при этом каждый раз исключается половина оставшихся чисел'''
 
-def game_core_v2(number):
     count = 1  # счетчик попыток
-    number = np.random.randint(1, 101)
-    bottom = 0
-    up = range(1, 101)[-1]
-    predict = int((bottom + up) / 2)
+    number = np.random.randint(1, 101) # загадываем случайное число в диапазоне от 1 до 100
+    bottom = 0 # нижняя граница интревала; изначально равна 0
+    up = range(1, 101)[-1] # верхняя граница интрвала; изначально равна 100
+    predict = int((bottom + up) / 2) # предиктор - в середине диапазона, изначально 50
 
     while number != predict:
-        count += 1
+        count += 1 # до тех пор пока не угадали число, считаем попытки
         if number > predict:
-            bottom = int(predict + 1)
-            predict = int((bottom + up) / 2)
+            bottom = int(predict + 1) # если число больше предиктора, отбрасываем половину чисел до предиктора
+            predict = int((bottom + up) / 2) # новый предиктор - середина нового диапазона
         elif number < predict:
-            up = int(predict - 1)
-            predict = int((bottom + up) / 2)
+            up = int(predict - 1) # если число меньше предиктора, отбрасываем половину чисел после предиктора
+            predict = int((bottom + up) / 2) # новый предиктор - середина нового диапазона
 
-    return count
+    return count, # если число угадано, возвращаем счетчик попыток
 
-score_game(game_core_v2)
+
+score_game(binary_search)
